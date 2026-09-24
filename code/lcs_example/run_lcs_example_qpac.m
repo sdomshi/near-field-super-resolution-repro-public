22761c7bd1ada57898332d286153ebd4a7527e8a
Sajad Daei <sajad2008se@yahoo.com>
2026-09-24 19:12:46 +0200

    Update run_lcs_example_qpac.m

--- code/lcs_example/run_lcs_example_qpac.m
+++ code/lcs_example/run_lcs_example_qpac.m
@@ -1,4 +1,4 @@
-% Self-contained reproducibility code for the LCS QPAC example.
+% Reproducibility code for the LCS QPAC example.
 % Paper: "A Mathematical Theory of Near-field Super-Resolution."
 % Author: Sajad Daei, KTH Royal Institute of Technology.
 % Email: sajado@kth.se
